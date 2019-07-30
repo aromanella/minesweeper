8dc0c575b0092e5b4e979709c6672b6555883cee
aromanella <aleromanella@gmail.com>
2019-07-30 18:23:49 -0300

    Spring Boot hookup class

---
+++ src/main/java/ar/com/romanella/minesweeper/BootUpSpringApp.java
@@ -0,0 +1,12 @@
+package ar.com.romanella.minesweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUpSpringApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootUpSpringApp.class, args);
+	}
+}
